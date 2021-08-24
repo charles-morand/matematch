3b2f6bf7ac7cd819b777ef7c11c451710cebbf03
Charles Morand <charles_morand@hotmail.fr>
2021-08-24 13:23:05 +0200

    draft seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+guide = User.create!(first_name: "John",
+                     last_name: "Doe",
+                     bio: "blablablablablablablablablablablablablablablablablablablablablablablablablablablabla",
+                     age: 19,
+                     birth_date: Date.new(2003, 5, 17),
+                     gender: "male",
+                     email: "john@example.com",
+                     password: "password",
+                     phone_number: "0102030405",
+                     role: "Guide",
+                     languages: ["FR", "GB"],
+                     hobbies: ["Tennis", "Football"],
+                     chosen_activities: ["Art"])
+
+explorer = User.create!(first_name: "Andrea",
+                        last_name: "Gia",
+                        bio: "blablablablablablablablablablablablablablablablablablablablablablablablablablablabla",
+                        age: 26,
+                        birth_date: Date.new(1995, 5, 17),
+                        gender: "female",
+                        email: "andrea@example.com",
+                        password: "password",
+                        phone_number: "0102030405",
+                        role: "Explorer",
+                        languages: ["FR", "ES"],
+                        hobbies: ["Skate", "Football"],
+                        chosen_activities: ["Art"])
