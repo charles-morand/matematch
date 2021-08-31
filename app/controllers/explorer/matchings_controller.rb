e3e7fcdbf4667751ddc4f2cd9d174b03ef05d6c2
tanguy deleage <tanguy.deleage0@gmail.com>
2021-08-31 12:59:35 +0200

    Réparation du matching

--- app/controllers/explorer/matchings_controller.rb
+++ app/controllers/explorer/matchings_controller.rb
@@ -1,22 +1,21 @@
 class Explorer::MatchingsController < ApplicationController
   def new
     @explorer_activities = User::ACTIVITIES
-    @selected_activity   = matching_criteria[:activity]
+    @selected_activity   = params[:activity]
   end
 
   def index
-    hobbies   = matching_criteria[:hobbies]
-    languages = matching_criteria[:languages]
-    city      = matching_criteria[:city]
-    ages      = matching_criteria[:age]
+    hobbies   = params[:hobbies]
+    languages = params[:languages]
+    city      = params[:city]
+    ages      = params[:age]
 
     @matched_users = User.
                       where(role: "Guide").
                       where("hobbies && ARRAY[?]::varchar[] AND languages &&  ARRAY[?]::varchar[]", hobbies, languages).
-                      where(address: city).
+                      where('address ILIKE ?', "%#{city}%").
                       where("TRUNC(EXTRACT('epoch' FROM AGE(NOW(), users.birth_date)) / 31557600) BETWEEN ? AND ?", *ages)
-  end
-
+                    end
   def show
     @matched_user = User.find(params[:id])
     @contact_request = ContactRequest.new
