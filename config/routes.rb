825800e83384ff56d64f2a57df935a5faba4c3ff
eric thanadabouth <ericthanadabouth@yahoo.fr>
2021-08-31 11:53:07 +0200

    Bon pour Home all et Home visiteur

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   devise_for :users
 
-  unauthenticated :user do
+  # unauthenticated :user do
     root to: 'pages#home'
-  end
+  # end
 
-  authenticate :user, ->(user) { user.role == "Explorer" } do
-    root to: "explorer/dashboards#index", as: :explorer_root
-  end
+  # authenticate :user, ->(user) { user.role == "Explorer" } do
+  #   root to: "explorer/dashboards#index", as: :explorer_root
+  # end
 
-  authenticate :user, ->(user) { user.role == "Guide" } do
-    root to: "guide/dashboards#show", as: :guide_root
-  end
+  # authenticate :user, ->(user) { user.role == "Guide" } do
+  #   root to: "guide/dashboards#show", as: :guide_root
+  # end
 
   resource :profile, only: [:show, :edit, :update]
 
   namespace :guide do
+    resource :dashboard, only: [:show]
     resource :chosen_activities, only: [:edit, :update]
     resources :contact_requests, only: [:index, :show] do
       member do
@@ -26,6 +27,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   end
 
   namespace :explorer do
+    resource :dashboard, only: [:show]
     resources :activities, only: [:index]
     resources :matchings, only: [:new, :index]
     resources :matchings, only: [:show]
