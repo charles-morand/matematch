42ad690c01119a24c0325b10729d6977aafb61e3
Charles Morand <charles_morand@hotmail.fr>
2021-08-31 17:26:43 +0200

    chosen_activities controller created

---
+++ app/controllers/guide/chosen_activities_controller.rb
@@ -0,0 +1,3 @@
+class Guide::ChosenActivitiesController < ApplicationController
+
+end
---
+++ test/controllers/chosen_activities_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChosenActivitiesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
