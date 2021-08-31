2aa4a55375c6bc1143827c3bd8379c3a74ef669a
Charles Morand <charles_morand@hotmail.fr>
2021-08-31 11:42:48 +0200

    activities added to the dashboard controller - the activities appear in the popup - the close button is removed in the footer

--- app/controllers/guide/dashboards_controller.rb
+++ app/controllers/guide/dashboards_controller.rb
@@ -2,6 +2,7 @@ class Guide::DashboardsController < ApplicationController
 
   def show
     @received_requests = current_user.received_contact_requests
+    @activities = User::ACTIVITIES
   end
 
   def accept
