c5d64c084657afe37302590db19fed59ec54decb
Charles Morand <charles_morand@hotmail.fr>
2021-09-01 15:13:41 +0200

    Twilio working fine

--- app/controllers/guide/contact_requests_controller.rb
+++ app/controllers/guide/contact_requests_controller.rb
@@ -1,8 +1,15 @@
 class Guide::ContactRequestsController < ApplicationController
+  TwilioClient = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
   def accept
     contact_request = ContactRequest.find(params[:id])
     contact_request.status = "Accepted"
     contact_request.save
+    TwilioClient.messages
+                .create(
+                        body: "#{current_user.first_name} accepted your request. His/her number is #{current_user.phone_number}",
+                        from: '+14783948317',
+                        to: "#{contact_request.explorer.phone_number}"
+                        )
     redirect_to guide_dashboard_path
   end
 
@@ -10,6 +17,12 @@ class Guide::ContactRequestsController < ApplicationController
     contact_request = ContactRequest.find(params[:id])
     contact_request.status = "Declined"
     contact_request.destroy
+    TwilioClient.messages
+                .create(
+                        body: "#{current_user.first_name} declined your request... :(",
+                        from: '+14783948317',
+                        to: "#{contact_request.explorer.phone_number}"
+                        )
     redirect_to guide_dashboard_path
   end
 end
--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -1,8 +1,22 @@
 class ContactRequest < ApplicationRecord
+  TwilioClient = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
   MATCHING_CRITERIA = %i[activity hobbies languages city age]
 
   belongs_to :guide, class_name: "User"
   belongs_to :explorer, class_name: "User"
   validates :status, presence: true
   validates :activity, presence: true
+
+  after_create :send_message
+
+  private
+
+  def send_message
+    TwilioClient.messages
+                .create(
+                         body: 'An explorer wants to match with you on Matematch!',
+                         from: '+14783948317',
+                         to: "#{self.guide.phone_number}"
+                       )
+  end
 end
